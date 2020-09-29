3354d197a023d0f117683dd27e5822aedab3d333
Abrar-Ur-Rahman <67436981+AbrarUr-Rahman@users.noreply.github.com>
2020-09-30 01:46:20 +0600

    Add files via upload

---
+++ TESTCODE/ConnectionTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author USER
+ */
+public class ConnectionTest {
+    Connection con;
+   
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void connectionTest() {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/house_rental_system";  
+            Properties info = new Properties();
+            info.put("user", "root");
+            info.put("password", "");
+            con = DriverManager.getConnection(url, info);
+            assertEquals(true,true);
+        }
+        catch(ClassNotFoundException | SQLException ex)
+        {
+            assertEquals(true,false);
+        }
+ 
+    }
+    @Test
+    public void connectionTest2() {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/house_rental_system";  
+            Properties info = new Properties();
+            info.put("user", "root");
+            info.put("password", "");
+            con = DriverManager.getConnection(url, info);
+            assertEquals(true,true);
+        }
+        catch(ClassNotFoundException | SQLException ex)
+        {
+            assertEquals(true,false);
+        }
+ 
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
---
+++ TESTCODE/DBConnectTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.*;
+import Model.DBConnect;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author USER
+ */
+public class DBConnectTest {
+    DBConnect db;
+   
+    
+    @Before
+    public void setUp() throws Exception {
+     db=Mockito.mock(DBConnect.class);
+    }
+    
+    @Test
+    public void testgetEmail()throws Exception{
+        when(db.getEmail("dont@gmal.com")).thenReturn("abrar@gmail.com");
+        assertEquals("abrar@gmail.com",db.getEmail("dont@gmail.com"));
+        
+    }
+    @Test
+    public void testgetAddress()throws Exception{
+        when(db.getAddress("221 Baker Street")).thenReturn("45 downton abbey");
+        assertEquals("45 downton abbey",db.getAddress("221 Baker Street"));
+        
+    }
+      @Test
+    public void testgetName()throws Exception{
+        when(db.getName("Ishmam")).thenReturn("abrar");
+        assertEquals("abrar",db.getName("Ishmam"));
+        
+    }
+        @Test
+    public void testlogin()throws Exception{
+        when(db.login("abrar","12345")).thenReturn(true);
+        assertEquals(true,db.login("abrar","12345"));
+        
+    }
+         @Test
+    public void testusertype()throws Exception{
+        when(db.userType("abrar")).thenReturn("client");
+        assertEquals("client",db.userType("abrar"));
+         }
+         @Test
+    public void testContact()throws Exception{
+        when(db.getContact("12345689")).thenReturn("2345678");
+        assertEquals("2345678",db.getContact("123456789"));
+         }
+         @Test
+    public void testcountShout()throws Exception{
+        when(db.countShout("number")).thenReturn(45);
+        assertEquals(45,db.countShout("number"));
+         }
+    
+}
